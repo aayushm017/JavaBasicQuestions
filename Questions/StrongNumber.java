da53ad889351713b51bcfc2c53976d231eff964b
aayushm017 <aayushm017@gmail.com>
2022-10-29 14:32:43 +0530

    .

---
+++ Questions/StrongNumber.java
@@ -0,0 +1,28 @@
+package Questions;
+import java.util.Scanner;
+
+public class StrongNumber {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter number to find it's a Strong number or not? = ");
+        int n = sc.nextInt();
+        int sum = 0, rem;
+        int original_n = n;
+
+        while(n != 0){
+            int fact = 1;
+            rem = n % 10;
+            for(int  i = rem; i >= 1; i--){
+                fact = fact * i;
+            }
+            sum = sum + fact;
+            n = n / 10;
+        }
+
+        if(original_n == sum){
+            System.out.println(sum + " = is a Strong Number");
+        }else{
+            System.out.println(sum + " = is not a Strong Number");
+        }
+    }
+}
